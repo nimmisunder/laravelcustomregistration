77f4c9be3d8b58dc053fa21c7373565ecdc61ecf
nimmisunder <nimmis.aobs@gmail.com>
2025-03-07 10:45:41 +0530

    Initial commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class LoginController extends Controller
+class  LoginController extends Controller
 {
     public function showLoginForm()
     {
